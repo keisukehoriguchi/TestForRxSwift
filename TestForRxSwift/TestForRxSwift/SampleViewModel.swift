8cb5507a812f6cbecec2f63354e7f4dc70059db6
hor1guch1ke1suke <hor1guch1ke1suke@yahoo.co.jp>
2021-07-01 23:29:05 +0900

    ObserbableのMerge系

--- TestForRxSwift/TestForRxSwift/SampleViewModel.swift
+++ TestForRxSwift/TestForRxSwift/SampleViewModel.swift
@@ -8,17 +8,69 @@
 import Foundation
 import RxSwift
 
-class Hoge {
-    
+class SampleViewModel {
+
+    //Sample1
     //Privateで発行するObservableを持つ
     private let eventSubject = PublishSubject<Int>()
-    
-    //発行したものを公開する用のプロパティを持つ
-    var event: Observable<Int> { return eventSubject }
-    
+
+//    //発行したものを公開する用のプロパティを持つ
+//    var event: Observable<Int> { return eventSubject }
+
     //Funcを動かした際に発行するようのものを同時に動かし、値を渡せるようにする
     func doSomething() {
         eventSubject.onNext(1)
+    }
+    
+    //Sample2
+    private let buttonHiddenSubject = BehaviorSubject(value: false)
+    var buttonHidden:Observable<Bool> { return buttonHiddenSubject }
+    
+    func start() {
+        buttonHiddenSubject.onNext(true)
+    }
+    
+    func stop() {
+        buttonHiddenSubject.onNext(false)
+    }
+    
+    func doAnything() {
+        addEvent.onNext(3)
+    }
+    
+    //Sample3 マージさせてみる
+    private let addEvent = PublishSubject<Int>()
+    
+    var event: Observable<Int> {
+        return Observable.of(eventSubject, addEvent).merge()
+    }
+    
+    private let locationMonitor = LocationMonitor()
+    private let route = Route()
+    
+    //Sample4
+    var distance: Observable<String> {
+        return Observable.combineLatest(locationMonitor.location, route.route) {loc, rto in
+            String(loc) + rto
+        }
+    }
+    
+}
+
+
+class LocationMonitor {
+    private let locationSubject = BehaviorSubject<Int>(value: 1)
+    
+    var location: Observable<Int> {
+        return locationSubject
+    }
+}
+
+class Route {
+    private let routeSubject = BehaviorSubject<String>(value: "100")
+    
+    var route: Observable<String> {
+        return routeSubject
     }
     
 }
--- TestForRxSwift/TestForRxSwift/ViewController.swift
+++ TestForRxSwift/TestForRxSwift/ViewController.swift
@@ -7,18 +7,30 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     
-    let hoge: Hoge = Hoge()
-    var disposable: Disposable = Hoge().event.subscribe()
+    let sampleViewModel: SampleViewModel = SampleViewModel()
+    var disposable: Disposable = SampleViewModel().event.subscribe()
+//    var boolDisposable: Disposable = SampleViewModel().buttonHidden.subscribe()
+    //bindの場合の初期化例
+    var boolDisposable: Disposable = SampleViewModel().buttonHidden.bind { Bool in
+    }
+    
+    //VC開放時に開放する場合
+    private let disposeBag = DisposeBag()
+    
     @IBOutlet weak var rxButton: UIButton!
     @IBOutlet weak var rxLabel: UILabel!
+    @IBOutlet weak var fukkatsuBtn: UIButton!
+    @IBOutlet weak var moreBtn: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        disposable = hoge.event.subscribe(
+        disposable = sampleViewModel.event.subscribe(
             onNext: { value in
                 self.rxLabel.text = String(value)
             }, onError: { error in
@@ -27,10 +39,37 @@ class ViewController: UIViewController {
                 print("completed")
             }
         )
+        
+//        boolDisposable = sampleViewModel.buttonHidden.subscribe(onNext: { bool in
+//            self.rxButton.isHidden = bool
+//        })
+        
+//        //bind使ってみる
+//        boolDisposable = sampleViewModel.buttonHidden.bind(to: rxButton.rx.isHidden)
+        
+        //直接Bindできる？？→機能した。じゃあもうDisposableいらないのでは？？
+        sampleViewModel.buttonHidden
+            .bind(to: rxButton.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        //同じ値を別にもBind
+        sampleViewModel.buttonHidden
+            .bind(to: moreBtn.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        
     }
     
     @IBAction func pressBtn(_ sender: Any) {
-        hoge.doSomething()
+        sampleViewModel.doSomething()
+        sampleViewModel.start()
+    }
+    @IBAction func fukkatsuClicked(_ sender: Any) {
+        sampleViewModel.stop()
+    }
+    
+    @IBAction func moreClicked(_ sender: Any) {
+        sampleViewModel.doAnything()
     }
     
 }
