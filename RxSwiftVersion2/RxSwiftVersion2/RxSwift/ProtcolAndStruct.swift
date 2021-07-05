fb79702f1606f55b98b9ec90ce6b8455edce0156
hor1guch1ke1suke <hor1guch1ke1suke@yahoo.co.jp>
2021-07-05 23:36:16 +0900

    RxVersion1

---
+++ RxSwiftVersion2/RxSwiftVersion2/RxSwift/ProtcolAndStruct.swift
@@ -0,0 +1,19 @@
+//
+//  ProtcolAndStruct.swift
+//  RxSwiftVersion2
+//
+//  Created by Keisuke Horiguchi on 2021/07/05.
+//
+
+import RxSwift
+import RxCocoa
+
+struct CounterViewModelInput {
+    let countUpButton: Observable<Void>
+    let countDownButton: Observable<Void>
+    let countResetButton: Observable<Void>
+}
+
+protocol CounterViewModelOutput {
+    var counterText: Driver<String?> { get }
+}
