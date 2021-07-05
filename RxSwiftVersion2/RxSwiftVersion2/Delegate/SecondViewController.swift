4ed86f4c394da204176aac3408398839d2b4d321
hor1guch1ke1suke <hor1guch1ke1suke@yahoo.co.jp>
2021-07-05 23:10:04 +0900

    Delegateの実装中

---
+++ RxSwiftVersion2/RxSwiftVersion2/Delegate/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  RxSwiftVersion2
+//
+//  Created by Keisuke Horiguchi on 2021/07/05.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
